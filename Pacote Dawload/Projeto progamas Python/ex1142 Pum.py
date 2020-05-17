fa160e9a8fbe5bb7e1c5540eafbd68da18d23735
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-05-17 12:33:10 -0300

    finished challenge

--- Pacote Dawload/Projeto progamas Python/ex1142 Pum.py
+++ Pacote Dawload/Projeto progamas Python/ex1142 Pum.py
@@ -1,5 +1,12 @@
 #ex1142 Pum
-
+entrada = int(input())                              # Quantidades de linhas de saida. ex: 7 linhas
+numero = 1                                          # variavel de controle a ser mostrada na tela
+for c in range(1, entrada + 1):        
+    print('{} {} {} PUM'.format(numero, numero + 1, numero + 2))# 1 2 3(segue a sequencia 4 em 4 p/baixo)       
+    numero = numero + 4                             # numero começa com 1 e cresce de 4 em 4 (7 linhas) ex:
+    
+        
+    
 
 
 
