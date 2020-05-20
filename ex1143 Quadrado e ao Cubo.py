c95c2a4c238ded9631b092469c52a6a16da4e71d
wagnersistemalima <wagner.sistemalima@gmail.com>
2020-05-19 21:09:21 -0300

    Create ex1143 Quadrado e ao Cubo.py

---
+++ ex1143 Quadrado e ao Cubo.py
@@ -0,0 +1,5 @@
+
+numero = int(input())                       # quantidade de linhas a ser mostrada
+for c in range(1, numero + 1):              # c é o contador que elevado ão Quadrado e ão cubo dar a sequência
+    print('{} {} {}'.format(c, c**2, c**3))
+    
